94e73e02db13cc53a5e5b71eba9384311abc94c6
xieyuen <107105640+xieyuen@users.noreply.github.com>
2023-07-12 16:37:05 +0800

    Update console.py

--- console.py
+++ console.py
@@ -2,27 +2,39 @@
 import os
 import sys
 
-# 定义全局变量
-__vars = {
+# 全局变量
+global_vars = {
     'path': os.getcwd(),
-    'echo': 'on',
-    'version': '0.0.1',
-    'license': 'MIT License',
+    'console': {
+        'echo': 'on',
+        'version': '0.0.1',
+        'license': 'MIT License',
+    },
     'help': {
         'console': {},
         'plugin': {}
     },
-    'cmd': {
-        'space': {
-            'count': 0,
-            'index': []
+    'temp': {
+        'cmd': {
+            'space': {
+                'count': 0,
+                'index': []
+            }
         }
-    }
+    },
 }
-__vars['welcome'] = 'Welcome to use this console!'
+global_vars['welcome'] = 'Welcome to use this console!'
+# 日志颜色，将 global_vars['console']['logger'] 设为 'default' 则为默认值
+global_vars['console']['logger'] = { 
+    'debug':    '\033[34m',
+    'info':     '\033[92m',
+    'error':    '\033[91m',
+    'warning':  '\033[93m',
+    'critical': '\033[31;1m'
+}
 # 命令帮助
-__vars['help']['console']['baseCmd'] = '::: Help :::\nConsole help massage:\n\ncommands:\ncd <path> : 改变工作路径\ncls : 清空屏幕\ndir : 列出目录下的所有文件(夹)\nls : 列出目录下的所有文件(夹)\necho *args : 回显\nhelp : 显示此页面，后加命令名可获取该命令的详细帮助\npause : pause console\npython : 调用 python 命令/函数/方法\nversion : 版本信息\nexit : 退出控制台\n'
-__vars['help']['console']['logger'] = '简单的日志工具\n\n直接使用这些命令：\n1. log_debug    调试\n2. log_info     信息\n3. log_warning  警告\n4. log_error    错误\n5. log_critical 严重'
+global_vars['help']['console']['baseCmd'] = '::: Help :::\nConsole help massage:\n\ncommands:\ncd <path> : 改变工作路径\ncls : 清空屏幕\ndir : 列出目录下的所有文件(夹)\nls : 列出目录下的所有文件(夹)\necho *args : 回显\nhelp : 显示此页面，后加命令名可获取该命令的详细帮助\npause : pause console\npython : 调用 python 命令/函数/方法\nversion : 版本信息\nexit : 退出控制台\n'
+global_vars['help']['console']['logger'] = '简单的日志工具\n\n直接使用这些命令：\n1. log.debug    调试\n2. log.info     信息\n3. log.warning  警告\n4. log.error    错误\n5. log_critical 严重'
 
 
 # 自定义函数
@@ -32,151 +44,185 @@ class ConsoleExit(Exception):
         self.message = message
     def __str__(self):
         return self.message
-    def __repr__(self):
-        return self.message
 
+# ---------------------------------------------------------------------------------------------
 
 class Logger:
     '''简单的日志工具'''
+    global global_vars
     # log 类型
+    __log_color = global_vars['console']['logger']
+    if __log_color == 'default': #默认值
+        __log_color = global_vars['console']['logger'] = { # 日志颜色
+            'debug':    '\033[34m',
+            'info':     '\033[92m',
+            'error':    '\033[91m',
+            'warning':  '\033[93m',
+            'critical': '\033[31;1m'
+        }
     __log_map = {
-        'info': '[Info]',
-        'error': '[ERROR]',
-        'warning': '[WARNING]',
-        'debug': '[debug]',
-        'critical': '[CRITICAL]'
+        'debug':    __log_color['debug']    + '[debug]',
+        'info':     __log_color['info']     + '[Info]',
+        'error':    __log_color['error']    + '[ERROR]',
+        'warning':  __log_color['warning']  + '[WARNING]',
+        'critical': __log_color['critical'] + '[CRITICAL]'
     }
     def __init__(self, __level: str):
         self.level = __level
     def __call__(self, message: str):
         __level = self.__log_map[self.level]
-        print(__level + ' ' + message)
+        print(__level + ' ' + message + '\033[0m')
 
+class LogOutput:
+    '''日志集成'''
+    def __init__(self):
+        self.debug = Logger('debug')
+        self.info = Logger('info')
+        self.warning = Logger('warning')
+        self.error = Logger('error')
+        self.critical = Logger('critical')
+    def debug(self, msg): self.debug(msg)
+    def info(self, msg): self.info(msg)
+    def warning(self, msg): self.warning(msg)
+    def warn(self, msg): self.warning(msg)
+    def error(self, msg): self.error(msg)
+    def critical(self, msg): self.critical(msg)
+# 生成实例
+log = LogOutput()
 
-# 这些都是已经实例化好的日志工具
-log_debug = Logger('debug')
-log_info = Logger('info')
-log_warning = Logger('warning')
-log_error = Logger('error')
-log_critical = Logger('critical')
-
+# ---------------------------------------------------------------------------------------------
 
 # 定义命令
-def __cmd_help(__other_cmd: str):
-    log_info('帮助')
-    if __other_cmd.isspace() or __other_cmd == '':
-        print(__vars['help']['console'])
-    else: ...
+class ConsoleCmd:
+    '''控制台命令'''
+    def help(__other_cmd: str):
+        log.info('帮助')
+        if __other_cmd.isspace() or __other_cmd == '':
+            log.info('Help Massage:')
+            print(global_vars['help']['console'])
+        else:
+            if __other_cmd in global_vars['help']:
+                log.info(global_vars['help'][__other_cmd])
+            else: log.warning(f'未找到 {__other_cmd } 的帮助！')
+        return     
 
-def __cmd_cd(__other_cmd: str):
-    global __vars
-    if __other_cmd.isspace() or __other_cmd == '':
-        log_info(__vars['path'])
-    else:
-        __path = __vars['path'] + '/' + __other_cmd
-        __vars['path'] = __path
-        if not os.path.exists(__path):
-            log_error('目录不存在')
-            os.mkdir(__path)
-            log_debug('目录已创建')
-        os.chdir(__path)
-        log_info('工作目录已更改为 ' + __vars['path'] + '.')
+    def cd(__other_cmd: str):
+        global global_vars
+        if __other_cmd.isspace() or __other_cmd == '':
+            log.info(global_vars['path'])
+        else:
+            __path = global_vars['path'] + '/' + __other_cmd
+            global_vars['path'] = __path
+            if not os.path.exists(__path):
+                log.error('目录不存在')
+                os.mkdir(__path)
+                log.debug('目录已创建')
+            os.chdir(__path)
+            log.info('工作目录已更改为 ' + global_vars['path'] + '.')
 
-def __cmd_mkdir(__other_cmd: str):
-    if __other_cmd.isspace() or __other_cmd == '':
-        log_error('mkdir 必须要指定目录名称')
-    else:
-        os.mkdir(__other_cmd)
-        log_info('目录已创建')
+    def mkdir(__other_cmd: str):
+        if __other_cmd.isspace() or __other_cmd == '':
+            log.error('mkdir 必须要指定目录名称')
+        else:
+            os.mkdir(__other_cmd)
+            log.info('目录已创建')
 
-def __cmd_cls(__none):
-    cmd('cls')
+    def cls(__none):
+        cmd('cls')
 
-def __cmd_dir(__none):
-    cmd('dir')
+    def dir(__none):
+        print(cmd('dir'))
 
-def __cmd_rmdir(__other_cmd: str):
-    if __other_cmd.isspace() or __other_cmd == '':
-        log_error('rmdir 必须要指定目录名称')
-    else:
-        os.rmdir(__other_cmd)
-        log_info('目录已删除')
+    def rmdir(__other_cmd: str):
+        if __other_cmd.isspace() or __other_cmd == '':
+            log.error('rmdir 必须要指定目录名称')
+        else:
+            os.rmdir(__other_cmd)
+            log.info('目录已删除')
 
-def __cmd_pause(__none):
-    print('请按 Enter 键继续...')
-    input()
+    def pause(__none):
+        log.info('请按 Enter 键继续...')
+        input()
 
-def __cmd_python(__other_cmd: str):
-    cmd('python ' + __other_cmd)
+    def version(__none):
+        log.info(global_vars['version'])
 
-def __cmd_version(__none):
-    log_info(__vars['version'])
+    def license(__none):
+        log.info(global_vars['license'])
 
-def __cmd_license(__none):
-    log_info(__vars['license'])
+    def echo(__other_cmd: str):
+        print(__other_cmd)
 
-def __cmd_echo(__other_cmd: str):
-    print(__other_cmd)
+    def exit(__none):
+        log.info('Bye!')
+        raise ConsoleExit
 
-def __cmd_exit(__none):
-    log_info('Bye!')
-    raise ConsoleExit
+    def plg(__other_cmd: str):
+        log.info('plugin')
+        print(__other_cmd)
+        log.info(global_vars['plugin'])
 
-def __eg_plg(__none):
-    print('ExamplePlugin')
+command_map = {
+    'exit':    ConsoleCmd.exit,
+    'quit':    ConsoleCmd.exit,
+    'help':    ConsoleCmd.help,
+    'cd':      ConsoleCmd.cd,
+    'md':      ConsoleCmd.mkdir,
+    'mkdir':   ConsoleCmd.mkdir,
+    'rd':      ConsoleCmd.rmdir,
+    'rmdir':   ConsoleCmd.rmdir,
+    'pause':   ConsoleCmd.pause,
+    'version': ConsoleCmd.version,
+    'license': ConsoleCmd.license,
+    'echo':    ConsoleCmd.echo,
+    'clear':   ConsoleCmd.cls,
 
-__command_map = {
-    'help': __cmd_help, 
-    'cd': __cmd_cd,
-    'md': __cmd_mkdir,
-    'mkdir': __cmd_mkdir,
-    'rd': __cmd_rmdir,
-    'rmdir': __cmd_rmdir,
-    'pause': __cmd_pause,
-    'python': __cmd_python,
-    'version': __cmd_version,
-    'license': __cmd_license,
-    'echo': __cmd_echo,
-    'clear': __cmd_cls,
-    'cls': __cmd_cls,
-    'dir': __cmd_dir,
-    'ls': __cmd_dir,
-    'exit': __cmd_exit,
-    'quit': __cmd_exit,
-    'ExamplePlugin': __eg_plg,
+    'plugin': ConsoleCmd.plg
 }
 
+def command_choice(header: str, other):
+    __action = command_map.get(header, None)
+    if __action: __action(other)
+    else:
+        log.warning(f'Command {header} not found')
+
+# ---------------------------------------------------------------------------------------------
 
 def analysis_space(string: str):
-    
-    global __vars
-    
+
+    global global_vars
+    __space = global_vars['temp']['cmd']['space']
+
     for __index in range(len(string)):
         __char = string[__index]
-        match __char:
-            case ' ':
-                __vars['cmd']['space']['count'] += 1
-                __vars['cmd']['space']['index'].append(__index)
-    if __vars['cmd']['space']['count'] == 0:
-        __vars['cmd']['space']['index'].append(len(string))
+        if __char == ' ':
+            __space['count'] += 1
+            __space['index'].append(__index)
+    if __space['count'] == 0:
+        __space['index'].append(len(string))
+
+    global_vars['temp']['cmd']['space'] = __space
+    return __space
 
 
 def analysis_cmd(command: str):
 
-    global __vars
-    global __command_map
+    global global_vars
+    global command_map
+    __1st_space = analysis_space(command)['index'][0]
 
-    analysis_space(command)
-    # 第一个空格
-    __action = __command_map.get(command[0:__vars['cmd']['space']['index'][0]], None)
-    if __action:
-        __action(command[__vars['cmd']['space']['index'][0] + 1:])
-    else:
-        log_warning('command not found')
+    # __action = __command_map.get(command[0:__1st_space], None)
+    # if __action:
+    #     __action(command[__1st_space + 1:])
+    # else:
+    #     log.warning('command not found')
 
+    del __action
+
+# ---------------------------------------------------------------------------------------------
 
 def main():
-    print(__vars['welcome'])
+    log.info(global_vars['welcome'])
     while True:
         input_str = input('>>> ')
         analysis_cmd(input_str)
