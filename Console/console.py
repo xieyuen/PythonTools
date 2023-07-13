dcd3ecd7dade2170a4ce6c9d6bdeabb95790ac05
xieyuen <107105640+xieyuen@users.noreply.github.com>
2023-07-13 18:31:37 +0800

    Update console.py

--- Console/console.py
+++ Console/console.py
@@ -1,6 +1,7 @@
 # 自己做一个控制台
 import os
 import sys
+import shutil
 
 # 全局变量
 global_vars = {
@@ -25,70 +26,90 @@ global_vars = {
 }
 global_vars['welcome'] = 'Welcome to use this console!'
 # 日志颜色，将 global_vars['console']['logger'] 设为 'default' 则为默认值
-global_vars['console']['logger'] = { 
-    'debug':    '\033[34m',
-    'info':     '\033[92m',
-    'error':    '\033[91m',
-    'warning':  '\033[93m',
-    'critical': '\033[31;1m'
+global_vars['console']['logger'] = {
+    'debug':    '\033[34m', # 蓝色
+    'info':     '\033[92m', # 绿色
+    'error':    '\033[91m', # 橙色
+    'warning':  '\033[93m', # 红色
+    'critical': '\033[31;1m' # 红色加粗
 }
 # 命令帮助
 global_vars['help']['console']['baseCmd'] = '::: Help :::\nConsole help massage:\n\ncommands:\ncd <path> : 改变工作路径\ncls : 清空屏幕\ndir : 列出目录下的所有文件(夹)\nls : 列出目录下的所有文件(夹)\necho *args : 回显\nhelp : 显示此页面，后加命令名可获取该命令的详细帮助\npause : pause console\npython : 调用 python 命令/函数/方法\nversion : 版本信息\nexit : 退出控制台\n'
-global_vars['help']['console']['logger'] = '简单的日志工具\n\n直接使用这些命令：\n1. log.debug    调试\n2. log.info     信息\n3. log.warning  警告\n4. log.error    错误\n5. log_critical 严重'
-
-
-# 自定义函数
-def cmd(command: str): os.system(command)
-class ConsoleExit(Exception):
-    def __init__(self, message: str = 'Console has exited.'):
-        self.message = message
-    def __str__(self):
-        return self.message
+global_vars['help']['console']['logger'] = '简单的日志工具\n\n直接使用这些命令：\n1. console.logger.debug    调试\n2. console.logger.info     信息\n3. console.logger.warning  警告\n4. console.logger.error    错误\n5. log_critical 严重'
 
 # ---------------------------------------------------------------------------------------------
 
-class Logger:
-    '''简单的日志工具'''
-    global global_vars
-    # log 类型
-    __log_color = global_vars['console']['logger']
-    if __log_color == 'default': #默认值
-        __log_color = global_vars['console']['logger'] = { # 日志颜色
-            'debug':    '\033[34m',
-            'info':     '\033[92m',
-            'error':    '\033[91m',
-            'warning':  '\033[93m',
-            'critical': '\033[31;1m'
-        }
-    __log_map = {
-        'debug':    __log_color['debug']    + '[debug]',
-        'info':     __log_color['info']     + '[Info]',
-        'error':    __log_color['error']    + '[ERROR]',
-        'warning':  __log_color['warning']  + '[WARNING]',
-        'critical': __log_color['critical'] + '[CRITICAL]'
-    }
-    def __init__(self, __level: str):
-        self.level = __level
-    def __call__(self, message: str):
-        __level = self.__log_map[self.level]
-        print(__level + ' ' + message + '\033[0m')
+class Console:
+    '''控制台工具'''
+    class Logger:
+        '''日志工具'''
+        class LogPrint:
+            '''日志打印'''
+            global global_vars
+            __log_color = global_vars['console']['logger']
+            if __log_color == 'default': # 默认
+                __log_color = global_vars['console']['logger'] = { # 日志颜色
+                    'debug':    '\033[34m', # 蓝色
+                    'info':     '\033[92m', # 绿色
+                    'error':    '\033[91m', # 橙色
+                    'warning':  '\033[93m', # 红色
+                    'critical': '\033[31;1m' # 红色加粗
+                }
+            __log_map = {
+                'debug':    __log_color['debug']    + '[debug]',
+                'info':     __log_color['info']     + '[Info]',
+                'error':    __log_color['error']    + '[ERROR]',
+                'warning':  __log_color['warning']  + '[WARNING]',
+                'critical': __log_color['critical'] + '[CRITICAL]'
+            }
+            def __init__(self, __level: str):
+                self.level = __level
+            def __call__(self, message: str):
+                __level = self.__log_map[self.level]
+                if self.level == 'debug' or self.level == 'info':
+                    print(__level + '\033[0m' + ' ' + message)
+                else: print(__level + ' ' + message + '\033[0m')
+        def __init__(self): # 实例化
+            self.__debug = self.LogPrint('debug')
+            self.__info = self.LogPrint('info')
+            self.__warning = self.LogPrint('warning')
+            self.__error = self.LogPrint('error')
+            self.__critical = self.LogPrint('critical')
 
-class LogOutput:
-    '''日志集成'''
+        def debug(self, msg): self.__debug(msg)
+        def info(self, msg): self.__info(msg)
+        def warning(self, msg): self.__warning(msg)
+        def warn(self, msg): self.__warning(msg)
+        def error(self, msg): self.__error(msg)
+        def critical(self, msg): self.__critical(msg)
+        def crit(self, msg): self.__critical(msg)
+        def catch_exc(self, msg): self.__info('\033[91m[CatchExc]\033[0m ' + msg)
+        def exception(self, msg, exc_msg):
+            '''这会打印 critical 级别的日志再抛出 ConsoleException 异常'''
+            self.critical(msg)
+            raise ConsoleException(exc_msg)
+
+    class CustomCmds:
+        '''自定义命令'''
+        def __init__(self): ...
+        def cmd(self, command: str): os.system(command)
+
+    class ConsoleException(Exception):
+        '''控制台异常，请在抛出异常前用 exc_msg() 方法设置异常信息'''
+        def __init__(self, msg: str = 'Console has exited.'):
+            self.message = msg
+        def __str__(self):
+            return self.message
+        def exc_msg(self, msg: str):
+            self.message = msg
+
     def __init__(self):
-        self.debug = Logger('debug')
-        self.info = Logger('info')
-        self.warning = Logger('warning')
-        self.error = Logger('error')
-        self.critical = Logger('critical')
-    def debug(self, msg): self.debug(msg)
-    def info(self, msg): self.info(msg)
-    def warning(self, msg): self.warning(msg)
-    def warn(self, msg): self.warning(msg)
-    def error(self, msg): self.error(msg)
-    def critical(self, msg): self.critical(msg)
-# 生成实例
-log = LogOutput()
+        self.logger = self.Logger()
+        self.custom_cmds = self.CustomCmds()
+        self.exception = self.ConsoleException()
+
+console = Console()
+cmd = console.custom_cmds.cmd
 
 # ---------------------------------------------------------------------------------------------
 
@@ -96,75 +117,98 @@ log = LogOutput()
 class ConsoleCmd:
     '''控制台命令'''
     def help(__other_cmd: str):
-        log.info('帮助')
+        console.logger.info('帮助')
         if __other_cmd.isspace() or __other_cmd == '':
-            log.info('Help Massage:')
+            console.logger.info('Help Massage:')
             print(global_vars['help']['console'])
         else:
             if __other_cmd in global_vars['help']:
-                log.info(global_vars['help'][__other_cmd])
-            else: log.warning(f'未找到 {__other_cmd } 的帮助！')
-        return     
+                console.logger.info(global_vars['help'][__other_cmd])
+            else: console.logger.warning(f'未找到 {__other_cmd } 的帮助！')
+        return None
 
     def cd(__other_cmd: str):
         global global_vars
         if __other_cmd.isspace() or __other_cmd == '':
-            log.info(global_vars['path'])
-        else:
-            __path = global_vars['path'] + '/' + __other_cmd
-            global_vars['path'] = __path
-            if not os.path.exists(__path):
-                log.error('目录不存在')
-                os.mkdir(__path)
-                log.debug('目录已创建')
-            os.chdir(__path)
-            log.info('工作目录已更改为 ' + global_vars['path'] + '.')
+            console.logger.info(global_vars['path'])
+            return global_vars['path']
+
+        __path = __other_cmd
+        if not os.path.exists(__path):
+            console.logger.error('目录不存在')
+            os.mkdir(__path)
+            console.logger.debug('目录已创建')
+        os.chdir(__path)
+        global_vars['path'] = os.getcwd()
+        console.logger.info('工作目录已更改为 ' + global_vars['path'] + '.')
+        return True
 
     def mkdir(__other_cmd: str):
         if __other_cmd.isspace() or __other_cmd == '':
-            log.error('mkdir 必须要指定目录名称')
-        else:
-            os.mkdir(__other_cmd)
-            log.info('目录已创建')
-
-    def cls(__none):
-        cmd('cls')
-
-    def dir(__none):
-        print(cmd('dir'))
+            console.logger.error('mkdir 必须要指定目录名称')
+            return False
+        os.mkdir(__other_cmd)
+        console.logger.info('目录已创建')
+        return True
 
     def rmdir(__other_cmd: str):
         if __other_cmd.isspace() or __other_cmd == '':
-            log.error('rmdir 必须要指定目录名称')
+            console.logger.error('rmdir 必须要指定目录名称')
         else:
-            os.rmdir(__other_cmd)
-            log.info('目录已删除')
+            try:
+                os.rmdir(__other_cmd)
+                console.logger.info('目录已删除')
+            except OSError:
+                console.logger.error('目录非空')
+                console.logger.error('是否强制删除目录？这会会删除目录下的所有文件(夹)')
+                ans = input('输入 y 确认，其他任意键取消：')
+                if ans == 'y':
+                    try:
+                        shutil.rmtree(__other_cmd)
+                        console.logger.info('目录已删除')
+                        return True
+                    except:
+                        # console.logger.catch_exc('Python 抛出了一个异常')
+                        console.logger.error('目录删除失败')
+                console.logger.info('目录删除操作取消')
+            except: console.logger.catch_exc('好像触发了什么奇奇怪怪的异常？')
+
+    def del_(__other_cmd: str):
+        if __other_cmd.isspace() or __other_cmd == '':
+            console.logger.error('del 必须要指定文件名称')
+            return False
+        if not os.path.exists(__other_cmd):
+            console.logger.error('文件不存在')
+        try: os.remove(__other_cmd)
+        except: console.logger.catch_exc('好像触发了什么奇奇怪怪的异常？')
+        else:
+            console.logger.info('文件已删除')
+            return True
 
     def pause(__none):
-        log.info('请按 Enter 键继续...')
+        console.logger.info('请按 Enter 键继续...')
         input()
 
     def version(__none):
-        log.info(global_vars['version'])
+        console.logger.info(global_vars['version'])
 
-    def license(__none):
-        log.info(global_vars['license'])
+    def license_(__none):
+        console.logger.info(global_vars['console']['license'])
 
-    def echo(__other_cmd: str):
-        print(__other_cmd)
+    def echo_(__other_cmd: str):
+        print(rf'{__other_cmd}')
 
-    def exit(__none):
-        log.info('Bye!')
-        raise ConsoleExit
+    def exit_(__none):
+        console.logger.info('Bye!')
+        console.exception.exc_msg('Console has exited.')
+        raise console.exception
 
     def plg(__other_cmd: str):
-        log.info('plugin')
+        console.logger.info('plugin')
         print(__other_cmd)
-        log.info(global_vars['plugin'])
+        console.logger.info(global_vars['plugin'])
 
 command_map = {
-    'exit':    ConsoleCmd.exit,
-    'quit':    ConsoleCmd.exit,
     'help':    ConsoleCmd.help,
     'cd':      ConsoleCmd.cd,
     'md':      ConsoleCmd.mkdir,
@@ -173,18 +217,12 @@ command_map = {
     'rmdir':   ConsoleCmd.rmdir,
     'pause':   ConsoleCmd.pause,
     'version': ConsoleCmd.version,
-    'license': ConsoleCmd.license,
+    'license': ConsoleCmd.license_,
     'echo':    ConsoleCmd.echo,
-    'clear':   ConsoleCmd.cls,
+    'del':     ConsoleCmd.del_,
 
     'plugin': ConsoleCmd.plg
 }
-
-def command_choice(header: str, other):
-    __action = command_map.get(header, None)
-    if __action: __action(other)
-    else:
-        log.warning(f'Command {header} not found')
 
 # ---------------------------------------------------------------------------------------------
 
@@ -210,21 +248,23 @@ def analysis_cmd(command: str):
     global global_vars
     global command_map
     __1st_space = analysis_space(command)['index'][0]
+    __action = command_map.get(command[0:__1st_space], None)
 
-    # __action = __command_map.get(command[0:__1st_space], None)
-    # if __action:
-    #     __action(command[__1st_space + 1:])
-    # else:
-    #     log.warning('command not found')
+    if __action:
+        __action(command[__1st_space + 1:])
+    else:
+        console.logger.warning('command not found')
 
     del __action
 
 # ---------------------------------------------------------------------------------------------
 
 def main():
-    log.info(global_vars['welcome'])
+    console.logger.info(global_vars['welcome'])
     while True:
         input_str = input('>>> ')
+        if input_str == 'exit' or input_str == 'quit' or input_str == '':
+            ConsoleCmd.exit_(None)
         analysis_cmd(input_str)
 
 
