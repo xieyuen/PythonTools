f45fb365db08ddcce875bb7c8a8fa82e859a41e8
xieyuen <107105640+xieyuen@users.noreply.github.com>
2023-08-23 12:38:15 +0800

    新增 MCDRPlugin@TpManager-v0.3.3 源码

---
+++ MCDR-Plugins/MCDR_Plg-Source-Code/TpManager-v0.3.3/tpm/commands.py
@@ -0,0 +1,143 @@
+
+import mcdreforged.api.all as MCDR
+
+from kpi.command import *
+
+from .globals import *
+from .utils import *
+from .api import *
+
+Prefix = '!!tp'
+TpaPrefix = '!!tpa'
+TphPrefix = '!!tph'
+
+def register(server: MCDR.PluginServerInterface):
+	cfg = get_config()
+	server.register_help_message(Prefix, 'TP manager help message')
+	server.register_help_message(TpaPrefix, 'Teleport to player')
+	server.register_help_message(TphPrefix, 'Teleport player to you')
+
+	Commands(Prefix, config=cfg).register_to(server)
+
+	server.register_command(
+		require_player(
+			cfg.require_permission(
+				MCDR.Literal(TpaPrefix), 'ask')).
+		redirects(Commands.ask.base))
+	server.register_command(
+		require_player(
+			cfg.require_permission(
+				MCDR.Literal(TphPrefix), 'askhere')).
+			redirects(Commands.askhere.base))
+
+class Commands(PermCommandSet):
+	Prefix = Prefix
+	HelpMessage = 'TP manager help message'
+
+	def __init__(self, *args, config, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.__config = config
+		self.__tpask_map = {}
+		self.__tpsender_map = {}
+
+	@property
+	def config(self):
+		return self.__config
+
+	def has_permission(self, src: MCDR.CommandSource, literal: str) -> bool:
+		return self.config.has_permission(src, literal)
+
+	def help(self, source: MCDR.CommandSource):
+		send_message(source, BIG_BLOCK_BEFOR, tr('help_msg', Prefix), BIG_BLOCK_AFTER, sep='\n')
+
+	@Literal('pos')
+	@player_only
+	def tppos(self, source: MCDR.PlayerCommandSource, x: float, y: float, z: float):
+		server = source.get_server()
+		player = source.player
+		cfg = get_config()
+		cmd = cfg.teleport_xyz_command.format(name=player, x=x, y=y, z=z)
+		server.execute(cmd)
+
+	@Literal('ask')
+	@player_only
+	def ask(self, source: MCDR.PlayerCommandSource, target: str):
+		server = source.get_server()
+		name = source.player
+		cfg = get_config()
+		# TODO: check the target player exists
+		if not self.register_accept(source, target,
+			lambda: [server.execute(c.format(src=name, dst=target)) for c in cfg.teleport_commands],
+			lambda: send_message(source, MSG_ID, MCDR.RText(tr('ask.aborted'), color=MCDR.RColor.red)),
+			lambda: send_message(source, MSG_ID, MCDR.RText(tr('ask.timeout'), color=MCDR.RColor.red)),
+			timeout=cfg.teleport_expiration):
+			return
+		send_message(source, MSG_ID, tr('ask.sending', target),
+			new_command('{} cancel'.format(Prefix), '[{}]'.format(tr('word.cancel')), color=MCDR.RColor.yellow))
+		server.tell(target, join_rtext(MSG_ID, tr('ask.request_to', name),
+			new_command('{} accept'.format(Prefix), '[{}]'.format(tr('word.accept')), color=MCDR.RColor.light_purple),
+			new_command('{} reject'.format(Prefix), '[{}]'.format(tr('word.reject')), color=MCDR.RColor.red),
+		))
+
+	@Literal('askhere')
+	@player_only
+	def askhere(self, source: MCDR.PlayerCommandSource, target: str):
+		server = source.get_server()
+		name = source.player
+		cfg = get_config()
+		# TODO: check the target player exists
+		if not self.register_accept(source, target,
+			lambda: [server.execute(c.format(src=target, dst=name)) for c in cfg.teleport_commands],
+			lambda: send_message(source, MSG_ID, MCDR.RText(tr('ask.aborted'), color=MCDR.RColor.red)),
+			lambda: send_message(source, MSG_ID, MCDR.RText(tr('ask.timeout'), color=MCDR.RColor.red)),
+			timeout=cfg.teleport_expiration):
+			return
+		send_message(source, MSG_ID, tr('ask.sending', target),
+			new_command('{} cancel'.format(Prefix), '[{}]'.format(tr('word.cancel')), color=MCDR.RColor.yellow))
+		server.tell(target, join_rtext(MSG_ID, tr('ask.request_from', name),
+			new_command('{} accept'.format(Prefix), '[{}]'.format(tr('word.accept')), color=MCDR.RColor.light_purple),
+			new_command('{} reject'.format(Prefix), '[{}]'.format(tr('word.reject')), color=MCDR.RColor.red),
+		))
+
+	@Literal('accept')
+	@player_only
+	def accept(self, source: MCDR.PlayerCommandSource):
+		self.__tpask_map.pop(source.player,
+			(lambda s: send_message(s, MCDR.RText(tr('word.no_action'), color=MCDR.RColor.red)), 0) )[0](source)
+
+	@Literal('reject')
+	@player_only
+	def reject(self, source: MCDR.PlayerCommandSource):
+		self.__tpask_map.pop(source.player,
+			(0, lambda s: send_message(s, MCDR.RText(tr('word.no_action'), color=MCDR.RColor.red))) )[1](source)
+
+	@Literal('cancel')
+	@player_only
+	def cancel(self, source: MCDR.PlayerCommandSource):
+		self.__tpsender_map.pop(source.player,
+			lambda s: send_message(s, MCDR.RText(tr('word.no_action'), color=MCDR.RColor.red)))(source)
+
+	def register_accept(self, source: MCDR.PlayerCommandSource, target: str,
+		accept_call, reject_call=lambda: 0,
+		timeout_call=lambda: 0, timeout: int = None):
+		assert isinstance(source, MCDR.PlayerCommandSource)
+		assert callable(accept_call)
+		assert callable(reject_call)
+		assert callable(timeout_call)
+		if target in self.__tpask_map:
+			send_message(source, MSG_ID, MCDR.RText(tr('ask.player_req_exists', target), color=MCDR.RColor.red))
+			return False
+		name = source.player
+		if name in self.__tpsender_map:
+			send_message(source, MSG_ID, MCDR.RText(tr('ask.req_exists'), color=MCDR.RColor.red))
+			return False
+
+		tmc = (lambda: 0) if timeout is None else new_timer(timeout,
+			lambda: (self.__tpask_map.pop(target), self.__tpsender_map.pop(name), timeout_call())
+		).cancel
+		canceler = lambda: (tmc(), self.__tpsender_map.pop(name))
+		self.__tpask_map[target] = (
+			lambda *args: (canceler(), dyn_call(accept_call, *args)),
+			lambda *args: (canceler(), dyn_call(reject_call, *args)))
+		self.__tpsender_map[name] = lambda *args: (tmc(), self.__tpask_map.pop(target), dyn_call(reject_call, *args))
+		return True
---
+++ MCDR-Plugins/MCDR_Plg-Source-Code/TpManager-v0.3.3/tpm/globals.py
@@ -0,0 +1,48 @@
+
+from typing import Dict, List
+
+import mcdreforged.api.all as MCDR
+
+from kpi.config import *
+from .utils import *
+
+__all__ = [
+	'MSG_ID', 'BIG_BLOCK_BEFOR', 'BIG_BLOCK_AFTER', 'TPMConfig', 'get_config', 'init', 'destory'
+]
+
+MSG_ID = MCDR.RText('[TPM]', color=MCDR.RColor.light_purple)
+BIG_BLOCK_BEFOR = LazyData(lambda data:
+	MCDR.RText('------------ {0} v{1} ::::'.format(data.name, data.version), color=MCDR.RColor.gold))
+BIG_BLOCK_AFTER = LazyData(lambda data:
+	MCDR.RText(':::: {0} v{1} ============'.format(data.name, data.version), color=MCDR.RColor.gold))
+
+class TPMConfig(Config, msg_id=MSG_ID):
+	# 0:guest 1:user 2:helper 3:admin 4:owner
+	class minimum_permission_level(JSONObject):
+		pos: int     = 2
+		ask: int     = 1
+		askhere: int = 1
+		accept: int  = 1
+		reject: int  = 0
+		cancel: int  = 0
+	teleport_expiration: int = 10 # in seconds
+	teleport_commands: List[str] = [
+		'say Teleporting {src} to {dst} ...',
+		'tp {src} {dst}',
+	]
+	teleport_xyz_command: str = 'tp {name} {x} {y} {z}'
+
+def get_config():
+	return TPMConfig.instance
+
+def init(server: MCDR.PluginServerInterface):
+	global BIG_BLOCK_BEFOR, BIG_BLOCK_AFTER
+	metadata = server.get_self_metadata()
+	LazyData.load(BIG_BLOCK_BEFOR, metadata)
+	LazyData.load(BIG_BLOCK_AFTER, metadata)
+	TPMConfig.init_instance(server, load_after_init=True)
+
+def destory(server: MCDR.PluginServerInterface):
+	cfg = get_config()
+	if cfg is not None:
+		cfg.save()
