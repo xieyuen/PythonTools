2003f5a5a307f21eb76f4292908f2ecf86872e77
xieyuen <xieyuenol@outlook.com>
2025-08-07 01:44:56 +0800

    feat: 补充代码使其更加通用

--- src/pythontools/modeling/base.py
+++ src/pythontools/modeling/base.py
@@ -7,8 +7,13 @@ def std(data, ddof: Literal[0, 1] = 0):
     if hasattr(data, "std"):
         return data.std(ddof=ddof)
     n = len(data)
+    if n == 1:
+        return 0
 
-    return (np.sum(data ** 2) - n * mean(data)) / (n - ddof)
+    return np.sqrt(
+        (np.sum(x ** 2 for x in data))
+        / (n - ddof) - mean(data) ** 2
+    )
 
 
 def mean(data):
