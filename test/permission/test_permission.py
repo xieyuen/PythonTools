a028c543db7f49bb147b27ee3dfc0148d4c57639
xieyuen <xieyuenol@outlook.com>
2025-08-06 14:14:56 +0800

    fix: PermissionError -> BasePermissionError (#10)
    * rename: PermissionError -> BasePermissionError
    
    * rename: PermissionError -> BasePermissionError

--- test/permission/test_permission.py
+++ test/permission/test_permission.py
@@ -1,33 +1,39 @@
 import unittest
 from enum import Enum, auto
 from pythontools import Permission
+from pythontools.exceptions import PermissionReregisterError
 
 
 class Perms(Enum):
     READ = auto()
     WRITE = auto()
     DELETE = auto()
-    EDIT = READ | WRITE | DELETE
+    UPDATE = auto()
 
 
-Permission.register(*Perms)
+Permission.register(Perms.WRITE, Perms.DELETE, Perms.READ)
+p = Permission(Perms.READ, Perms.WRITE)
 
 
 class TestPermission(unittest.TestCase):
 
     def test_permission_operations(self):
-        # 创建一个包含READ和WRITE权限的Permission对象
-        p = Permission(Perms.READ, Perms.WRITE)
-
         # 测试权限存在性
         self.assertTrue(p.has(Perms.READ))
         self.assertTrue(p.has(Perms.WRITE))
         self.assertFalse(p.has(Perms.DELETE))
-        self.assertFalse(p.has(Perms.EDIT))
 
         # 测试移除权限
         p.remove(Perms.WRITE)
         self.assertFalse(p.has(Perms.WRITE))
+
+    def test_lock(self):
+        Permission.lock()
+        self.assertRaises(RuntimeError, Permission.register, Perms.UPDATE)
+        Permission.lock()
+
+    def test_reregister(self):
+        self.assertRaises(PermissionReregisterError, Permission.register, Perms.READ)
 
 
 if __name__ == "__main__":
